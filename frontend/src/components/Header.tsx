07153ea9f524d18a0d072070f88adc020affe518
정다니엘 <cdl2141@naver.com>
2025-06-30 15:52:42 +0900

    fix: App.tsx를 부모로 Home, Header 에 Props 전달

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,98 +1,19 @@
-import { useState } from "react";
+import type { ReactEventHandler } from "react";
 import { FaSearch } from "react-icons/fa";
 
-
-type Video = {
-    videoId: string;
-    thumbnail: string;
-    title: string;
-    description: string;
-    publishedAt: string;
-    channelTitle: string;
-    views: number;
-    performanceScore: number;
+type HeaderPropsType = {
+    keyword: string;
+    fetchSuggestions: (input: string) => void;
+    handleKeyDown: ReactEventHandler;
+    fetchVideos: (isNextPage?: boolean) => void;
+    suggestions: string[];
+    selectedIndex: number;
+    setSelectedIndex: React.Dispatch<React.SetStateAction<number>>;
+    setKeyword: React.Dispatch<React.SetStateAction<string>>;
+    setSuggestions: React.Dispatch<React.SetStateAction<string[]>>
 };
 
-const Header = () => {
-
-    // 검색 키워드 state
-    const [keyword, setKeyword] = useState<string>("");
-    //
-    const [suggestions, setSuggestions] = useState([]);
-    // 선택된 자동완성 항목
-    const [selectedIndex, setSelectedIndex] = useState(-1);
-
-    // 영상 정보 state
-        const [videos, setVideos] = useState<Video[]>([]);
-        //
-        const [nextPageToken, setNextPageToken] = useState<boolean | null>(null);
-        // 에러 state
-        const [error, setError] = useState<string>("");
-    
-        // 유튜브 영상 검색
-        const fetchVideos = async (isNextPage = false) => {
-            try {
-                const url = `http://43.203.72.105:80/api/videos?keyword=${keyword}${
-                    nextPageToken && isNextPage ? `&pageToken=${nextPageToken}` : ""
-                }`;
-                const response = await fetch(url);
-                const data = await response.json();
-    
-                if (response.ok) {
-                    setVideos(
-                        isNextPage ? [...videos, ...data.videos] : data.videos
-                    );
-                    setNextPageToken(data.nextPageToken || null);
-                    setError("");
-                } else {
-                    setError(data.error);
-                }
-            } catch (err: unknown) {
-                setError("서버 요청 실패");
-            }
-        };
-
-
-// ✅ 자동완성 API 호출 함수
-    const fetchSuggestions = async (input: string) => {
-        setKeyword(input);
-        setSelectedIndex(-1); // 입력할 때마다 선택 초기화 디바운싱
-
-        if (!input.trim()) {
-            setSuggestions([]);
-            return;
-        }
-        try {
-            const response = await fetch(
-                `http://43.203.72.105:80/api/autocomplete?keyword=${input}`
-            );
-            const data = await response.json();
-            setSuggestions(response.ok ? data.suggestions || [] : []);
-        } catch (err: unknown) {
-            setSuggestions([]);
-        }
-    };
-
-    // ✅ 키보드 이벤트 핸들러
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (suggestions.length === 0) return;
-
-        if (e.key === "ArrowDown") {
-            setSelectedIndex((prev) =>
-                prev < suggestions.length - 1 ? prev + 1 : prev
-            );
-        } else if (e.key === "ArrowUp") {
-            setSelectedIndex((prev) => (prev > 0 ? prev - 1 : prev));
-        } else if (e.key === "Enter") {
-            if (selectedIndex >= 0) {
-                setKeyword(suggestions[selectedIndex]);
-                setSuggestions([]);
-            }
-            fetchVideos();
-        }
-    };
-
-
+const Header: React.FC<HeaderPropsType> = ({ keyword, fetchSuggestions, handleKeyDown, fetchVideos, suggestions, setSelectedIndex, selectedIndex, setKeyword, setSuggestions }) => {
 
     return (
         <header className="fixed top-0 left-0 w-full h-20 bg-white border-b border-[#3aad6c] flex justify-between items-center px-6 z-10">
