882a83fc3436980f715cd226caaf16001ea1af6b
정다니엘 <cdl2141@naver.com>
2025-07-11 23:40:04 +0900

    refactor: 커스텀 훅으로 분리 / 코드 정리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,97 +2,26 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import "./App.css";
-import { useState } from "react";
 import Detail from "./pages/Detail";
-import type { VideosType } from "./types/youtube.type";
+import { useYoutubeData } from "./hooks/useYoutubeData";
 
 function App() {
-    // 커스텀 훅으로 로직 빼고 나머지 props로 전달
-
-    // 검색 키워드 state
-    const [keyword, setKeyword] = useState<string>("");
-    //
-    const [suggestions, setSuggestions] = useState<string[]>([]);
-    // 선택된 자동완성 항목
-    const [selectedIndex, setSelectedIndex] = useState(-1);
-
-    // 영상 정보 state
-    const [videos, setVideos] = useState<VideosType[]>([]);
-    //
-    const [nextPageToken, setNextPageToken] = useState<boolean | null>(null);
-    // 에러 state
-    const [error, setError] = useState<Error | null>(null);
-
-    // 유튜브 영상 검색
-    const fetchVideos = async (isNextPage = false) => {
-        try {
-            const url = `http://43.203.72.105:3000/api/videos?keyword=${keyword}${
-                nextPageToken && isNextPage ? `&pageToken=${nextPageToken}` : ""
-            }`;
-            const response = await fetch(url);
-            const data = await response.json();
-
-            if (response.ok) {
-                setVideos(
-                    isNextPage ? [...videos, ...data.videos] : data.videos
-                );
-                setNextPageToken(data.nextPageToken || null);
-                setError(null);
-            } else {
-                setError(data.error);
-            }
-        } catch (err: unknown) {
-            console.error(err);
-        }
-    };
-
-    // ✅ 자동완성 API 호출 함수
-    const fetchSuggestions = async (input: string) => {
-        setKeyword(input);
-        setSelectedIndex(-1); // 입력할 때마다 선택 초기화 디바운싱
-
-        if (!input.trim()) {
-            setSuggestions([]);
-            return;
-        }
-        try {
-            const response = await fetch(
-                `http://43.203.72.105:3000/api/autocomplete?keyword=${input}`
-            );
-            const data = await response.json();
-            setSuggestions(response.ok ? data.suggestions || [] : []);
-        } catch (err: unknown) {
-            setSuggestions([]);
-        }
-    };
-
-    // ✅ 키보드 이벤트 핸들러
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (suggestions.length === 0) return;
-
-        if (e.key === "ArrowDown") {
-            setSelectedIndex((prev) =>
-                prev < suggestions.length - 1 ? prev + 1 : prev
-            );
-        } else if (e.key === "ArrowUp") {
-            setSelectedIndex((prev) => (prev > 0 ? prev - 1 : prev));
-        } else if (e.key === "Enter") {
-            if (selectedIndex >= 0) {
-                setKeyword(suggestions[selectedIndex]);
-                setSuggestions([]);
-            }
-            fetchVideos();
-        }
-    };
-
-    // ✅ 성과도 점수를 5단계로 변환하는 함수
-    const getPerformanceLabel = (score: number) => {
-        if (score >= 90) return `Great 🚀 (${score})`;
-        if (score >= 70) return `Good 👍 (${score})`;
-        if (score >= 50) return `Normal 😐 (${score})`;
-        if (score >= 41) return `Bad 👎 (${score})`;
-        return `Worst ❌ (${score})`;
-    };
+    const {
+        keyword,
+        setKeyword,
+        suggestions,
+        setSuggestions,
+        selectedIndex,
+        setSelectedIndex,
+        fetchSuggestions,
+        handleKeyDown,
+        fetchVideos,
+        getPerformanceLabel,
+        videos,
+        setVideos,
+        nextPageToken,
+        error,
+    } = useYoutubeData();
 
     return (
         <div className="w-screen h-auto">
--- frontend/src/hooks/useYoutubeData.ts
+++ frontend/src/hooks/useYoutubeData.ts
@@ -1,3 +1,105 @@
-export const useYoutubeData = () => {
+import { useState } from "react";
+import type { VideosType } from "../types/youtube.type";
 
+export const useYoutubeData = () => {
+    // 검색 키워드 state
+    const [keyword, setKeyword] = useState<string>("");
+    // 자동 완성 state
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+    // 선택된 자동완성 항목
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    // 영상 정보 state
+    const [videos, setVideos] = useState<VideosType[]>([]);
+    // 데이터 더 보기 토큰 state
+    const [nextPageToken, setNextPageToken] = useState<boolean | null>(null);
+    // 에러 state
+    const [error, setError] = useState<Error | null>(null);
+
+    // 유튜브 영상 검색 함수
+    const fetchVideos = async (isNextPage = false) => {
+        try {
+            const url = `http://localhost:3000/api/videos?keyword=${keyword}${nextPageToken && isNextPage ? `&pageToken=${nextPageToken}` : ""
+                }`;
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if (response.ok) {
+                setVideos(
+                    isNextPage ? [...videos, ...data.videos] : data.videos
+                );
+                setNextPageToken(data.nextPageToken || null);
+                setError(null);
+            } else {
+                setError(data.error);
+            }
+        } catch (err: unknown) {
+            console.error(err);
+        }
+    };
+
+    // 자동완성 API 호출 함수
+    const fetchSuggestions = async (input: string) => {
+        setKeyword(input);
+        setSelectedIndex(-1); // 입력할 때마다 선택 초기화 디바운싱
+
+        if (!input.trim()) {
+            setSuggestions([]);
+            return;
+        }
+        try {
+            const response = await fetch(
+                `http://localhost:3000/api/autocomplete?keyword=${input}`
+            );
+            const data = await response.json();
+            setSuggestions(response.ok ? data.suggestions || [] : []);
+        } catch (err) {
+            console.error(err);
+            setSuggestions([]);
+        }
+    };
+
+    // 키보드 이벤트 핸들러
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (suggestions.length === 0) return;
+
+        if (e.key === "ArrowDown") {
+            setSelectedIndex((prev) =>
+                prev < suggestions.length - 1 ? prev + 1 : prev
+            );
+        } else if (e.key === "ArrowUp") {
+            setSelectedIndex((prev) => (prev > 0 ? prev - 1 : prev));
+        } else if (e.key === "Enter") {
+            if (selectedIndex >= 0) {
+                setKeyword(suggestions[selectedIndex]);
+                setSuggestions([]);
+            }
+            fetchVideos();
+        }
+    };
+
+    // ✅ 성과도 점수를 5단계로 변환하는 함수
+    const getPerformanceLabel = (score: number) => {
+        if (score >= 90) return `Great 🚀 (${score})`;
+        if (score >= 70) return `Good 👍 (${score})`;
+        if (score >= 50) return `Normal 😐 (${score})`;
+        if (score >= 41) return `Bad 👎 (${score})`;
+        return `Worst ❌ (${score})`;
+    };
+
+    return {
+        keyword,
+        setKeyword,
+        suggestions,
+        setSuggestions,
+        selectedIndex,
+        setSelectedIndex,
+        fetchSuggestions,
+        handleKeyDown,
+        fetchVideos,
+        getPerformanceLabel,
+        videos,
+        setVideos,
+        nextPageToken,
+        error
+    }
 }
