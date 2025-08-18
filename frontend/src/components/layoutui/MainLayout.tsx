dbbdb28d38e50c5dff24d737204ca3de606fff1a
정다니엘 <cdl2141@naver.com>
2025-08-18 18:59:08 +0900

    feat: mypage, analyze 페이지 skeleton 적용

--- frontend/src/components/layoutui/MainLayout.tsx
+++ frontend/src/components/layoutui/MainLayout.tsx
@@ -1,7 +1,7 @@
 const MainLayout = ({ children, className}: { children: React.ReactNode, className?:string }) => {
     return (
         <main className="w-full h-[100vh] px-24 pt-36 pb-20 bg-gray-50 flex-1">
-            <div className={`shadow-md w-full h-full flex flex-col justify-between items-center border border-primary rounded-3xl p-5 gap-5 ${className} overflow-hidden`}>
+            <div className={`shadow-md w-full h-full flex flex-col justify-start items-center border border-primary rounded-3xl p-5 gap-5 ${className} overflow-hidden`}>
                 {children}
             </div>
         </main>
